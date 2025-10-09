bbc28ca8622eec5780fe276921a6485e96f9be8e
Aaron Cordova <aron2014123456@gmail.com>
2025-10-08 22:01:24 -0600

    Nuvo boton

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -23,13 +23,26 @@ export function ProjectCard({ proyecto }: ProjectCardProps) {
         <CardTitle className="text-balance">{proyecto.nombreProyecto}</CardTitle>
         <CardDescription className="text-pretty line-clamp-2">{proyecto.descripcionProyecto}</CardDescription>
       </CardHeader>
-      <CardFooter>
+      <CardFooter className="flex gap-4">
         <Button asChild variant="outline" className="w-full bg-transparent">
           <a href={proyecto.url} target="_blank" rel="noopener noreferrer">
             <ExternalLink className="h-4 w-4 mr-2" />
             Ver Proyecto
           </a>
         </Button>
+
+        {proyecto.s3VideoKey !== null && (
+          <Button asChild className="w-full"> {/* Puedes usar el estilo por defecto o primary */}
+            <a 
+              href={`https://portafoliovideo.s3.us-east-1.amazonaws.com/${proyecto.s3VideoKey}`} // Reemplaza con la URL correcta para ver tu video
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <Video className="h-4 w-4 mr-2" />
+              Ver Video
+            </a>
+          </Button>
+        )}
       </CardFooter>
     </Card>
   )
--- lib/api.ts
+++ lib/api.ts
@@ -9,6 +9,7 @@ export interface Proyecto {
   urlImagen: string
   url: string
   disponibleProyecto?: boolean
+  s3VideoKey?:string
 }
 
 export interface CreateProyectoDto {
@@ -16,6 +17,7 @@ export interface CreateProyectoDto {
   descripcionProyecto: string
   urlImagen: string
   url: string
+  s3VideoKey?:string
 }
 
 // Proyectos públicos
