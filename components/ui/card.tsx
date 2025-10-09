e373eaa47ed33ca3a8ff9d5eedef3d4e558b436e
Aaron Cordova <aron2014123456@gmail.com>
2025-10-09 00:34:53 -0600

    Señor ayudame porfavis

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -73,11 +73,16 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
 
 // src/components/ui/card.tsx
 
+// src/components/ui/card.tsx
+
 function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="card-footer"
-      className={cn('flex items-center px-6 [.border-t]:pt-6', className)}
+      className={cn(
+        'flex items-center px-6 [.border-t]:pt-6 justify-start', // <-- AGREGAR: justify-start para un mejor layout
+        className,
+      )}
       {...props}
     />
   )
